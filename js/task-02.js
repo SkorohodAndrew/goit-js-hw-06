ab4d6e68e4e76327b80b651085d9113e3ddf9198
Skorohod Andrew <pmisa376@gmail.com>
2022-07-02 23:19:32 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = option;
+
+  return itemEl
+})
+
+ingredientsList.append(...elements)
+console.log(...elements)
+
+
+
+
+
+
+// const firstEl = document.createElement('li');
+// firstEl.classList.add('item');
+// firstEl.textContent = 'Potatoes';
+
+// const navEl = document.querySelector('#ingredients');
+// navEl.appendChild(firstEl);
+
+// console.log(navEl)
