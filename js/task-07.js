2a6dcd382a6c03255cd450a263673386b04ad9e3
Skorohod Andrew <pmisa376@gmail.com>
2022-07-04 21:26:44 +0300

    fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,12 @@
 const refs = {
     font: document.querySelector('#font-size-control'),
     text: document.querySelector('#text'),
-
 }
  
+
 refs.text.style.fontSize = refs.font.value + 'px';
 refs.font.addEventListener('input', onInputBlur);
+
 
 
 function onInputBlur(event) {
