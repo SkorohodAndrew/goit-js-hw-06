77fb0cf054d0c6885c8ab1a4e8dc30ae0d252e21
Skorohod Andrew <pmisa376@gmail.com>
2022-07-09 10:54:31 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,24 +1,29 @@
-const titleEl = document.querySelectorAll('h2');
-console.log('Number of categories:',titleEl.length);
-
-// titleEl.forEach((element) => {
-//     return console.log('Category:', element.textContent,
-//     'Elements:', element.textContent)
-    
-// });
-
 const categoryEl = document.querySelectorAll('h2');
+const elementsEl = document.querySelectorAll('.item ul');
 
-const ElementsEl = document.querySelectorAll('.item ul');
+console.log('Number of categories:',elementsEl.length);
 
-const firstEl = 
-    console.log('Category:', categoryEl[0].textContent)
-    console.log('Elements:', ElementsEl[0].children.length);
 
-const secondEl = 
-    console.log('Category:', categoryEl[1].textContent)
-    console.log('Elements:', ElementsEl[1].children.length);
+categoryEl.forEach((element) => {
+    console.log('Category:', element.textContent);
+    
+    elementsEl.forEach((element) => {
+        console.log('Elements:', element.children.length);
+        
+    });
+    
+});
 
-const thirdEl = 
-    console.log('Category:', categoryEl[2].textContent)
-    console.log('Elements:',ElementsEl[2].children.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
